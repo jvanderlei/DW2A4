5892d4bc983ab022b60b16dbc48a479f43d1df51
jvanderlei <79838953+jvanderlei@users.noreply.github.com>
2022-11-04 11:24:11 -0300

    Add files via upload

---
+++ aula5(0)/dist/bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _reactDom = require("react-dom");
+
+var _App = require("./App");
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+var root = document.getElementById('root');
+console.log(document);
+(0, _reactDom.render)( /*#__PURE__*/(0, _jsxRuntime.jsx)(_App.App, {}), root);
