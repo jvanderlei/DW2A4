20df2f88cd8fb78d2fdc60f7a1220b2fb7a9b8b1
jvanderlei <79838953+jvanderlei@users.noreply.github.com>
2022-11-11 08:51:23 -0300

    Working Prod

---
+++ projeto-spa/SPA/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.text ?? "Default"}</button>;
+}
+
+function App() {
+  return (
+    <div>
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
